476530afd9b2a14181268b10701c3c3679ca6392
Halil İbrahim Köse <halilkosee5@gmail.com>
2021-11-21 00:07:38 +0300

    upload
    upload file added

--- src/main/java/com/softwaresecuritytesting/softwaresecuritytesting/controller/FilesController.java
+++ src/main/java/com/softwaresecuritytesting/softwaresecuritytesting/controller/FilesController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,25 +19,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@CrossOrigin("http://localhost:8081")
 public class FilesController {
 
     @Autowired
     FilesStorageService storageService;
 
+
+    @GetMapping("/fileUpload")
+    public String homepage() {
+        return "fileUpload";
+    }
+
+
     @PostMapping("/upload")
-    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+    public Object uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes attributes) {
         String message = "";
         try {
             storageService.save(file);
-
+            attributes.addFlashAttribute("message", "You successfully uploaded " + file.getOriginalFilename() + '!');
             message = "Uploaded the file successfully: " + file.getOriginalFilename();
-            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+            return "redirect:/";
         } catch (Exception e) {
             message = "Could not upload the file: " + file.getOriginalFilename() + "!";
-            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+            attributes.addFlashAttribute("message", "Could not upload the file: " + file.getOriginalFilename() + '!');
+
+            return "redirect:/";
         }
     }
 
